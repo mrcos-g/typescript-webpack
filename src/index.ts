77fd83a71b0d17c74a34d87374edbf701de32e4a
mrcos-g <marcgonzalez@outlook.com>
2023-11-18 06:49:53 -0500

    include dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import http from 'http';
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+  require('dotenv').config();
+}
+
+import http, { Server } from 'http';
 
 const hostname = '127.0.0.1';
-const port = 4000;
+const port = Number(process.env.PORT) || 3000;
 
-const server = http.createServer((req, res) => {
+const server: Server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
